ac83b3a83db9694b21ff7e7a91987cdcd08c034d
snow <120940610@qq.com>
2018-03-29 23:55:41 +0800

    include ButterKnife ;Wallet passwd create and confirm partial devlp

--- module.wallet/src/main/java/com/loopr/wallet/wallet/ui/activity/PasswdConfirmActivity.java
+++ module.wallet/src/main/java/com/loopr/wallet/wallet/ui/activity/PasswdConfirmActivity.java
@@ -9,7 +9,6 @@ import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
-import com.alibaba.android.arouter.launcher.ARouter;
 import com.loopr.wallet.common.AppGlobal;
 import com.loopr.wallet.common.ui.activity.BaseActivity;
 import com.loopr.wallet.common.ui.widget.CommonTitleBar;
@@ -22,26 +21,21 @@ import butterknife.OnClick;
 import butterknife.OnTextChanged;
 
 /**
- * Created by snow on 2018/3/17.
+ * Created by snow on 2018/3/29.
  */
-@Route(path = "/wallet/CreateWalletActivity")
-public class CreateWalletActivity extends BaseActivity{
+@Route(path = "/wallet/PasswdConfirmActivity")
+public class PasswdConfirmActivity extends BaseActivity{
     @BindView(R2.id.title_bar)
     public CommonTitleBar commonTitleBar;
 
-    @BindView(R2.id.wallet_name)
-    public EditText mWalletName;
+    @BindView(R2.id.wallet_confirm)
+    public EditText mWalletConfirm;
 
-    @BindView(R2.id.wallet_name_line)
-    public View mWalletNameLine;
+    @BindView(R2.id.wallet_confirm_line)
+    public View mWalletConfirmLine;
 
-    @BindView(R2.id.wallet_secret)
-    public EditText mWalletSecret;
 
-    @BindView(R2.id.wallet_secret_line)
-    public View mWalletSecretLine;
-
-    @BindView(R2.id.wallet_next_img)
+    @BindView(R2.id.wallet_confirm_next)
     public ImageView mWalletNext;
 
     static final ButterKnife.Setter<View, Boolean> STATUS = new ButterKnife.Setter<View, Boolean>() {
@@ -57,12 +51,10 @@ public class CreateWalletActivity extends BaseActivity{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.wallet_create_activity);
+        setContentView(R.layout.wallet_passwd_confirm_activity);
         ButterKnife.bind(this);
 
-        ButterKnife.apply(mWalletNameLine,STATUS, !TextUtils.isEmpty(mWalletName.getText()));
-        ButterKnife.apply(mWalletSecretLine,STATUS,!TextUtils.isEmpty(mWalletSecret.getText()));
-
+        ButterKnife.apply(mWalletConfirmLine,STATUS, !TextUtils.isEmpty(mWalletConfirm.getText()));
         commonTitleBar.setLeftOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -72,20 +64,14 @@ public class CreateWalletActivity extends BaseActivity{
 
     }
 
-    @OnClick(R2.id.wallet_next_img)
+    @OnClick(R2.id.wallet_confirm_next)
     public void onNextClick(ImageView imageView){
-        ARouter.getInstance().build("/wallet/PasswdConfirmActivity").navigation();
 
     }
 
-    @OnTextChanged(value = R2.id.wallet_name, callback = OnTextChanged.Callback.AFTER_TEXT_CHANGED)
+    @OnTextChanged(value = R2.id.wallet_confirm, callback = OnTextChanged.Callback.AFTER_TEXT_CHANGED)
     void onWalletNameAfterTextChanged(Editable s) {
-        ButterKnife.apply(mWalletNameLine,STATUS, !TextUtils.isEmpty(s.toString()));
+        ButterKnife.apply(mWalletConfirmLine,STATUS, !TextUtils.isEmpty(s.toString()));
     }
 
-    @OnTextChanged(value =R2.id.wallet_secret, callback = OnTextChanged.Callback.AFTER_TEXT_CHANGED)
-    void onWalletSecretAfterTextChanged(Editable s) {
-        ButterKnife.apply(mWalletSecretLine,STATUS,!TextUtils.isEmpty(s.toString()));
-
-    }
 }
