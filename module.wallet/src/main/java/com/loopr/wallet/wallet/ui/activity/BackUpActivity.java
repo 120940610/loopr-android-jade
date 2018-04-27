7ca49ac962a0b2dd5a8e70a10d16fd600db3afe3
snow <120940610@qq.com>
2018-04-27 23:13:40 +0800

    write down page to backup

--- module.wallet/src/main/java/com/loopr/wallet/wallet/ui/activity/BackUpActivity.java
+++ module.wallet/src/main/java/com/loopr/wallet/wallet/ui/activity/BackUpActivity.java
@@ -2,10 +2,12 @@ package com.loopr.wallet.wallet.ui.activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
+import com.alibaba.android.arouter.launcher.ARouter;
 import com.loopr.wallet.common.ui.activity.BaseActivity;
 import com.loopr.wallet.common.ui.widget.CommonTitleBar;
 import com.loopr.wallet.wallet.R;
@@ -20,11 +22,40 @@ import butterknife.OnClick;
  */
 @Route(path = "/wallet/BackUpActivity")
 public class BackUpActivity extends BaseActivity{
+    @BindView(R2.id.title_bar)
+    public CommonTitleBar commonTitleBar;
+
+    @BindView(R2.id.wallet_verify)
+    public TextView mWalletVerify;
+
+    @BindView(R2.id.wallet_backup_move)
+    public TextView mWalletBackupMove;
+
+    @BindView(R2.id.grid_recycle)
+    public RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.wallet_backup_activity);
+        ButterKnife.bind(this);
+
+        commonTitleBar.setLeftOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+    @OnClick(R2.id.wallet_verify)
+    public void onVerifyClick(TextView textView){
+        //ARouter.getInstance().build("/wallet/BackUpActivity").navigation();
+    }
+
+    @OnClick(R2.id.wallet_backup_move)
+    public void onMoveClick(TextView textView){
+        //ARouter.getInstance().build("/wallet/BackUpActivity").navigation();
     }
 
 }
