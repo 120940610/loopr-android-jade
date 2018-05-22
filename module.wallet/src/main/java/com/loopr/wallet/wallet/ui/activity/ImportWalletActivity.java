4ffc929385d4614039b2b32bd6715f2b8bdde7b4
snow <120940610@qq.com>
2018-05-22 23:38:16 +0800

    change the Indicator width of the TabLayout

--- module.wallet/src/main/java/com/loopr/wallet/wallet/ui/activity/ImportWalletActivity.java
+++ module.wallet/src/main/java/com/loopr/wallet/wallet/ui/activity/ImportWalletActivity.java
@@ -2,15 +2,19 @@ package com.loopr.wallet.wallet.ui.activity;
 
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.util.Pair;
 import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.loopr.wallet.common.ui.activity.BaseActivity;
+import com.loopr.wallet.common.ui.widget.CommonTitleBar;
 import com.loopr.wallet.wallet.R;
 import com.loopr.wallet.wallet.R2;
 import com.loopr.wallet.wallet.entity.Constant;
@@ -22,6 +26,7 @@ import com.loopr.wallet.wallet.ui.fragment.ImportPrivateKeyFragment;
 import com.loopr.wallet.wallet.viewmodel.ImportWalletViewModel;
 import com.loopr.wallet.wallet.viewmodel.ImportWalletViewModelFactory;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -57,6 +62,9 @@ public class ImportWalletActivity extends BaseActivity{
     @BindView(R2.id.tabLayout)
     TabLayout tabLayout;
 
+    @BindView(R2.id.title_bar)
+    CommonTitleBar titleBar;
+
     @Inject
     ImportWalletViewModelFactory importWalletViewModelFactory;
     ImportWalletViewModel importWalletViewModel;
@@ -75,10 +83,18 @@ public class ImportWalletActivity extends BaseActivity{
 
         viewPager.setAdapter(new TabPagerAdapter(getSupportFragmentManager(), pages));
         tabLayout.setupWithViewPager(viewPager);
+        setIndicator(tabLayout,20,20);
 
         importWalletViewModel = ViewModelProviders.of(this, importWalletViewModelFactory)
                 .get(ImportWalletViewModel.class);
         importWalletViewModel.wallet().observe(this, this::onWallet);
+
+        titleBar.setLeftOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 
     @Override
@@ -103,6 +119,41 @@ public class ImportWalletActivity extends BaseActivity{
     public void onBackPressed() {
         setResult(RESULT_CANCELED);
         super.onBackPressed();
+    }
+
+
+    /**
+     * change the Indicator width of the TabLayout
+     */
+    public void setIndicator(TabLayout tabs, int leftDip, int rightDip) {
+        Class<?> tabLayout = tabs.getClass();
+        Field tabStrip = null;
+        try {
+            tabStrip = tabLayout.getDeclaredField("mTabStrip");
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+
+        tabStrip.setAccessible(true);
+        LinearLayout ll_tab = null;
+        try {
+            ll_tab = (LinearLayout) tabStrip.get(tabs);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        int left = (int) (Resources.getSystem().getDisplayMetrics().density * leftDip);
+        int right = (int) (Resources.getSystem().getDisplayMetrics().density * rightDip);
+
+        for (int i = 0; i < ll_tab.getChildCount(); i++) {
+            View child = ll_tab.getChildAt(i);
+            child.setPadding(0, 0, 0, 0);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.MATCH_PARENT, 1);
+            params.leftMargin = left;
+            params.rightMargin = right;
+            child.setLayoutParams(params);
+            child.invalidate();
+        }
     }
 
 
