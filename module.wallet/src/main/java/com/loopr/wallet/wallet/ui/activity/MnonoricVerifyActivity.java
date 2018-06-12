bd788f0a09fedb896b79fc77712ed9b3f8e839a7
snow <120940610@qq.com>
2018-06-12 09:32:34 +0800

    remove the old designed implementation

--- module.wallet/src/main/java/com/loopr/wallet/wallet/ui/activity/MnonoricVerifyActivity.java
+++ module.wallet/src/main/java/com/loopr/wallet/wallet/ui/activity/MnonoricVerifyActivity.java
@@ -5,26 +5,19 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.SparseArray;
 import android.view.View;
-import android.widget.TextView;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.loopr.wallet.common.ui.activity.BaseActivity;
 import com.loopr.wallet.common.ui.widget.CommonTitleBar;
-import com.loopr.wallet.common.ui.widget.ViewFlow;
 import com.loopr.wallet.common.utils.LogUtils;
 import com.loopr.wallet.wallet.R;
 import com.loopr.wallet.wallet.R2;
 import com.loopr.wallet.wallet.entity.Conf;
-import com.loopr.wallet.wallet.ui.adapter.ViewFlowAdapter;
 import com.loopr.wallet.wallet.util.RandomUtil;
-
-import java.util.HashSet;
-import java.util.Random;
 
 import butterknife.BindDrawable;
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 /**
  * Created by snow on 2018/5/27.
@@ -43,10 +36,6 @@ public class MnonoricVerifyActivity extends BaseActivity{
     @BindDrawable(R2.drawable.wallet_rectangle_white_bg)
     Drawable mWalletWhiteBg;
 
-    @BindView(R2.id.view_flow)
-    ViewFlow mViewFlow;
-
-    ViewFlowAdapter mViewFlowAdapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -54,14 +43,6 @@ public class MnonoricVerifyActivity extends BaseActivity{
         setContentView(R.layout.wallet_mn_verify_activity);
         ButterKnife.bind(this);
         initData();
-        mViewFlowAdapter=new ViewFlowAdapter(mViewFlow,this);
-        mViewFlow.setAdapter(mViewFlowAdapter);
-        mViewFlowAdapter.setData(Conf.mNPart);
-        mViewFlow.setOnViewSwitchListener(new ViewFlow.ViewSwitchListener() {
-            @Override
-            public void onSwitched(View view, int position) {
-            }
-        });
 
         commonTitleBar.setLeftOnClickListener(new View.OnClickListener() {
             @Override
